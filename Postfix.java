879e5f755c61d6d276ac326117d62da1c9f5500a
Mr-Lein <151097135+Mr-Lein@users.noreply.github.com>
2024-10-02 10:19:51 -0600

    Update Postfix.java

--- Postfix.java
+++ Postfix.java
@@ -1,1 +1,7 @@
-
+public class Postfix{
+  public static void main(String[] args){
+    //use a scanner to input your postfix expression. Use integers
+    //use a stack to help you evaluate your postfix expression
+    //print out just the answer to the postfix expression
+  }
+}
